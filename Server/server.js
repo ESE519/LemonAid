f32cf5a5f17f41512afdd89cf8f011c5a80c1ac7
pulahshah <pulahshah@gmail.com>
2012-11-17 04:57:40 -0500

    sample gauge working with connection from server

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,7 @@ $npm install -d socket.io
 
 */
 
+var speed = 0
 
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
@@ -13,27 +14,31 @@ var app = require('http').createServer(handler)
 app.listen(8080);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+  //fs.readFile(__dirname + '/index.html',
+  //function (err, data) {
+   // if (err) {
+     // res.writeHead(500);
+     // return res.end('Error loading index.html');
+   // }
 
+   // res.writeHead(200);
+   // res.end(data);
+  //});
+  
     res.writeHead(200);
-    res.end(data);
-  });
+	res.end(data);
 }
 
 io.sockets.on('connection', function (socket) {
 	console.log('*** Client connected ***' + socket.id);
 	
-	setTimeout(function(){
-		socket.emit('news', { hello: 'world' });
-  		console.log('*** news event emitted ***');
-  	}, 10000);
+		setInterval(function(){
+			speed = Math.floor((Math.random()*10)+1);
+			socket.emit('news', { hello: speed*2 });
+  			//console.log('*** news event emitted *** ');
+  		}, 300);
 	
-  	
+	
   	
   	socket.on('my other event', function (data) {
     	console.log('*** other event occured ***');
