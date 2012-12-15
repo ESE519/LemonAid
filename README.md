dfc23ffbea7459dc5d73a7ad88f32cf94d6f311e
Huang Yu <mathew.park.yu@gmail.com>
2012-12-15 16:24:23 -0500

    write setup lemonaid script

--- README.md
+++ README.md
@@ -11,4 +11,18 @@ and then clone our project by running this command: `git clone git://github.com/
 And the "install\_lemonaid.sh" will do everything for you.
 
 ###Beaglebone Setup
-After running the install-lemonaid script, you will have an customized Ubuntu with our pcandriver on board. After plugging the SD card into your Beaglebone, **power up your Beaglebone with a separate power cord**, and connect the board to your laptop through USB cable. 
+After running the install-lemonaid script, you will have an customized Ubuntu with our pcandriver on board. 
+After plugging the SD card into your Beaglebone, **power up your Beaglebone with a separate power cord**, 
+and connect the board to your laptop through USB cable. And run 
+`screen /dev/ttyUSBX 115200`   
+If you see no output, just type Enter, it will take 1 to 5 minutes to have the board bootup the operating system.  
+When you see the login prompt, login with username: "ubuntu", password: "temppwd".   
+Then you will be logged in onto the "home" path, and then run `sh ./setup_lemondaid.sh` to do the final setup
+
+###Run Program
+After setup of everything, run the following commands to start the standard program:
+`sudo canmonitor -f=/dev/pcanXX > /dev/null 2>&1 &`  *(depends on the pcan device name)*  
+`node server.js` *(depends on the file name)*  
+   
+   
+**For more information about the cansniffer programs, please refer to the Readme in cansniffer folder**
--- tools/setup_lemonaid.sh
+++ tools/setup_lemonaid.sh
@@ -1,1 +1,56 @@
 #!/bin/bash
+
+echo "installing required packages..."
+echo " "
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install gcc-4.6 g++-4.6 g++-4.6-multilib curl build-essential make libtinfo-dev libstdc++6 libstdc++6-4.6-dev hostapd dhcp3-server node mysql-server mysql-common mysql-client
+curl http://npmjs.org/install.sh | sh
+sudo npm install socket.io 
+sudo npm install -d mysql
+
+echo "configuring required packages..."
+echo " "
+
+echo "setting up hostapd..."
+WLAN_INTERFACE=`sudo ifconfig -a | grep wlan | awk '{print $1}'`
+sudo cp /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf_backup
+sudo echo "interface=${WLAN_INTERFACE}" > /etc/hostapd/hostapd.conf
+sudo echo "driver=nl80211" >> /etc/hostapd/hostapd.conf
+sudo echo "ssid=rttep" >> /etc/hostapd/hostapd.conf
+sudo echo "channel=1" >> /etc/hostapd/hostapd.conf
+sudo update-rc.d hostapd defaults
+
+echo "setting up dhcp3-server..."
+sudo cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf_backup
+sudo echo "ddns-update-style-none;" > /etc/dhcp/dhcpd.conf
+sudo echo "authoritative;" >> /etc/dhcp/dhcpd.conf
+sudo echo "subnet" >> /etc/dhcp/dhcpd.conf
+sudo echo "192.168.1.0 netmask 255.255.255.0 {" >> /etc/dhcp/dhcpd.conf
+sudo echo "option routers 192.168.1.1;" >> /etc/dhcp/dhcpd.conf
+sudo echo "option subnet-mask 255.255.255.0;" >> /etc/dhcp/dhcpd.conf
+sudo echo "option broadcast-address 192.168.1.255;" >> /etc/dhcp/dhcpd.conf
+sudo echo "option domain-name-servers 192.168.1.1;" >> /etc/dhcp/dhcpd.conf
+sudo echo "option range 192.168.1.2 192.168.1.10;" >> /etc/dhcp/dhcpd.conf
+sudo echo "option default-lease-time 1209600;" >> /etc/dhcp/dhcpd.conf
+sudo echo "option mas-lease-time 1814400;" >> /etc/dhcp/dhcpd.conf
+sudo echo "}" >> /etc/dhcp/dhcpd.conf
+sudo update-rc.d isc-dhcp-server defaults
+
+echo "setting up network interfaces..."
+sudo cp /etc/network/interfaces /etc/network/interfaces_backup
+sudo echo "auto lo" > /etc/network/interfaces
+sudo echo "iface lo inet loopback" >> /etc/network/interfaces
+sudo echo "auto wlan0" >> /etc/network/interfaces
+sudo echo "iface wlan0 inet static" >> /etc/network/interfaces
+sudo echo "address 192.168.1.1" >> /etc/network/interfaces
+sudo echo "netmask 255.255.255.0" >> /etc/network/interfaces
+sudo echo "gateway 192.168.1.1" >> /etc/network/interfaces
+
+echo "setting up can monitor program..."
+make clean && make
+
+echo "Done!"
+echo "Reboot required!"
+
